461acaebc37002e2e7b7820aa4d23243217765a6
Pierre-Alexis VALBRUN <pierre-alexis.valbrun@epita.fr>
2021-04-29 07:09:23 +0200

    Version soutenance solo : OK

--- Studio Hoshigaru/Assets/Scripts/DeadState.cs
+++ Studio Hoshigaru/Assets/Scripts/DeadState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 
 public class DeadState : MonoBehaviour
@@ -9,6 +10,7 @@ public class DeadState : MonoBehaviour
     Parallaxing parallaxing;
     public Canvas UI;
     private PhotonView PV;
+    bool can = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +26,13 @@ public class DeadState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PV.IsMine)
+        if (can)
+        {
+            Debug.Log("Test");
+            can = false;
+            PhotonNetwork.LoadLevel(4);
+        }
+        else if (PV.IsMine)
         {
             parallaxing.cam = DisplayCameraWhenDead();
         }
@@ -34,8 +42,9 @@ public class DeadState : MonoBehaviour
     public Transform DisplayCameraWhenDead()
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        if (players.Length == 0)
+        if (players.Length == 0 && !can)
         {
+            can = true;
             return null;
         }
         else
--- Studio Hoshigaru/Assets/Scripts/PlayerDeath.cs
+++ Studio Hoshigaru/Assets/Scripts/PlayerDeath.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System.IO;
-
+using UnityEngine.SceneManagement;
 public class PlayerDeath : MonoBehaviour
 {
     private Health health;
     private PhotonView PV;
-
+    public Animator animator;
+    public PlayerControler playerControler;
+    public CapsuleCollider2D capsuleCollider;
+    public Rigidbody2D rb;
+    public Camera camera;
+    bool isDead = false;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +27,24 @@ public class PlayerDeath : MonoBehaviour
     {
         if (PV.IsMine)
         {
-            Debug.Log(health.numOfHits);
-            Death();
+            if (health.numOfHits <= 0 && !isDead)
+            {
+                Debug.Log("yop");
+                isDead = true;
+                animator.SetTrigger("isDead");
+                playerControler.enabled = false;
+                gameObject.tag = "Untagged";
+            }
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 4)
+        {
+            Destroy(this.gameObject);
         }
     }
 
     public void Death()
     {
-        if (health.numOfHits <= 0)
-        {
-            PhotonNetwork.Instantiate(Path.Combine("Prefab", "Player", "DeadPlayer"), transform.position, Quaternion.identity, 0); ;
-            PhotonNetwork.Destroy(PV.gameObject);
-        }
+        camera.gameObject.SetActive(false);
+        PhotonNetwork.Instantiate(Path.Combine("Prefab", "Player", "DeadPlayer"), transform.position, Quaternion.identity, 0); ;
     }
 }
