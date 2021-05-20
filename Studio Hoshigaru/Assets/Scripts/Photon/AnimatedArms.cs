3270e696837fbfe8e5a2c5f54f48153dcc22cd0d
Pierre-Alexis VALBRUN <pierre-alexis.valbrun@epita.fr>
2021-05-20 14:18:28 +0200

    Fix minor bugs
    Point destroy on load
    Arrows

--- Studio Hoshigaru/Assets/Scripts/Photon/AnimatedArms.cs
+++ Studio Hoshigaru/Assets/Scripts/Photon/AnimatedArms.cs
@@ -1,31 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class AnimatedArms : MonoBehaviour
 {
     [SerializeField] private Camera camera;
     [HideInInspector] public Vector2 direction;
     [SerializeField] public PlayerControler playerControler;
+    public PhotonView PV;
 
     void Update()
     {
-        Vector2 bowPosition = transform.position;
-        Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-        if(playerControler.facingRight)
-            direction = new Vector2(bowPosition.x - mousePosition.x, bowPosition.y - mousePosition.y);
-        else
-            direction = new Vector2(mousePosition.x - bowPosition.x, mousePosition.y - bowPosition.y);
-        transform.right = direction;
-        if (transform.rotation.eulerAngles.z < 300 && transform.rotation.eulerAngles.z > 50)
+        if (PV.IsMine)
         {
-            if (transform.rotation.eulerAngles.z > 180)
-            {
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 300));
-            }
+            Vector2 bowPosition = transform.position;
+            Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            if (playerControler.facingRight)
+                direction = new Vector2(bowPosition.x - mousePosition.x, bowPosition.y - mousePosition.y);
             else
+                direction = new Vector2(mousePosition.x - bowPosition.x, mousePosition.y - bowPosition.y);
+            transform.right = direction;
+            if (transform.rotation.eulerAngles.z < 300 && transform.rotation.eulerAngles.z > 50)
             {
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 50));
+                if (transform.rotation.eulerAngles.z > 180)
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0, 0, 300));
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(0, 0, 50));
+                }
             }
         }
     }
--- Studio Hoshigaru/Assets/Scripts/Player/Bow.cs
+++ Studio Hoshigaru/Assets/Scripts/Player/Bow.cs
@@ -35,7 +35,6 @@ public class Bow : MonoBehaviour
             {
                 points[i].transform.position = PointPosition(i * spaceBetweenPoints);
             }
-            
         }
     }
 
