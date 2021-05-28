de330de92d35a61b62c9b52bba003462cca6e171
Benoit <benoit.burg@epita.fr>
2021-05-28 04:18:49 +0200

    UI ++ / Health Bar des Boss etc...

---
+++ Studio Hoshigaru/Assets/Scripts/Enemy/Health_Bar_Boss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_Bar_Boss : MonoBehaviour
+{
+    public Slider slider;
+
+    public GameObject Boss;
+
+    public int Health;
+
+    public void Start()
+    {
+        Health = Boss.GetComponent<EnemyHealth>().health;
+        SetMaxHealth(Health);
+    }
+
+    public void Update()
+    {
+        Health = Boss.GetComponent<EnemyHealth>().health;
+        SetHealth(Health);
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+}
+
