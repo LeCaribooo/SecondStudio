9ab8fa46c5d8045c41042f16b8a01c645b7ebf8b
Aishan mob <kerian.allaire@gmail.com>
2021-05-28 04:19:11 +0200

    Boss Warrior en ennemi normal
    Il est clean

---
+++ Studio Hoshigaru/Assets/Scripts/Enemy/GuardianWarrior/TriggerWarrior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWarrior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GuardianWarrior enemyParent;
+
+    private void Awake()
+    {
+        enemyParent = GetComponentInParent<GuardianWarrior>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            enemyParent.target = collider.transform;
+            enemyParent.hotZone.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            enemyParent.target = collider.transform;
+            enemyParent.hotZone.SetActive(true);
+        }
+    }
+}
