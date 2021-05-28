d3b19267c42896ede712f17f7f5749e0d039d0ea
Benoit <benoit.burg@epita.fr>
2021-05-28 14:39:26 +0200

    Systme de Salle de Boss

---
+++ Studio Hoshigaru/Assets/Scripts/BossFight/BossTemple.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class BossTemple : MonoBehaviourPun
+{
+
+
+    public bool RoomCleared = false;
+
+    public GameObject Block;
+
+    public BossTemplePortal BossTemplePortal;
+
+    public string newscene;
+
+
+
+
+    [SerializeField]
+    private GameObject[] spawnpoint = new GameObject[8];
+
+    [SerializeField]
+    private GameObject[] enemies = new GameObject[2];
+
+    private List<GameObject> mobwaves = new List<GameObject>();
+
+    //Timer
+    float time = 6f;
+    float endtime = 10f;
+
+
+    //==================\\
+
+    private void OnEnable()
+    {
+        Fill_mobwaves();
+        int spawn = 0;
+        foreach (GameObject mob in mobwaves)
+        {
+            Debug.Log("Spawn & Instantiate");
+            int spawnPoint = spawn;
+            string name = mob.name;
+            Debug.Log("Mob name :" + name);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.Instantiate
+                    (Path.Combine("Prefab", "Enemy", name), spawnpoint[spawnPoint].transform.position, Quaternion.identity);
+            }
+            spawn++;
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsClearBoss())
+        {
+            RoomCleared = true;
+
+            //Envoie des infos.
+            base.photonView.RPC("SendRoomCleared", RpcTarget.Others, RoomCleared);
+        }
+
+        if (IsClearMob())
+        {
+            Block.gameObject.SetActive(false);
+            //base.photonView.RPC("DestroyBlock", RpcTarget.All, Block);
+        }
+
+        if (RoomCleared)
+        {
+            BossTemplePortald.gameObject.SetActive(true);
+        }
+    }
+
+    //Rempli la liste mobwaves de tous les monstres.
+    private void Fill_mobwaves()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            int nb_mob = PhotonNetwork.PlayerList.Length;
+            Debug.LogWarning("Nombre de " + enemies[i].name + " : " + nb_mob);
+            for (int j = 0; j < nb_mob; j++)
+            {
+                mobwaves.Add(enemies[i]);
+            }
+            Debug.LogWarning("Apres le remplissage nombre de mob dans mobwaves: " + mobwaves.Count);
+        }
+    }
+
+
+
+
+    //Vérifie si la salle est clear de boss.
+    private bool IsClearBoss()
+    {
+        GameObject[] boss = GameObject.FindGameObjectsWithTag("Boss");
+        return boss.Length == 0;
+    }
+
+    private bool IsClearMob()
+    {
+        GameObject[] enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        return enemy.Length == 0;
+    }
+
+
+    //Envoie des info des vagues.
+
+    [PunRPC]
+    //RoomClear
+    void SendRoomCleared(bool roomclear)
+    {
+        RoomCleared = roomclear;
+    }
+
+    [PunRPC]
+    //RoomClear
+    void DestroyBlock(GameObject block)
+    {
+        block.gameObject.SetActive(false);
+    }
+}
---
+++ Studio Hoshigaru/Assets/Scripts/BossFight/BossTemplePortal.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BossTemplePortal : MonoBehaviourPun
+{
+    public string nameportal;
+
+    public BossTemple BossTemple;
+
+
+
+    public GameObject Player;
+    public Canvas Vote;
+    public Canvas Level;
+    public Button LevelVote;
+    public Text Chronotext;
+    public Text Count;
+    private bool HasClickE = false;
+    private bool ready = false;
+    public Button Ready;
+    [SerializeField] Text readyUpText;
+    private bool WantToTeleport = false;
+
+    private List<bool> playerReady = new List<bool>();
+
+
+
+    //Timer
+    float time = 21f;
+
+
+    GameObject getMinePlayer()
+    {
+        GameObject[] joueur = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < joueur.Length; i++)
+        {
+            if (joueur[i].GetPhotonView().IsMine)
+            {
+                return joueur[i];
+            }
+        }
+        //Pas censé arrivé.
+        Debug.Log("Tu n'existes pas...");
+        return null;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player" && collision.gameObject == getMinePlayer())
+        {
+            Level.gameObject.SetActive(true);
+            if (HasClickE || WantToTeleport)
+            {
+                Level.gameObject.SetActive(false);
+                LevelVote.gameObject.SetActive(false);
+            }
+
+
+
+
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.gameObject == getMinePlayer())
+        {
+            Level.gameObject.SetActive(false);
+        }
+    }
+
+    public void Click_ToVote()
+    {
+
+        Level.gameObject.SetActive(false);
+        GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var joueur in player)
+        {
+            DontDestroyOnLoad(joueur);
+        }
+
+        SendNotif();
+
+        VoteCanvas();
+
+    }
+    private void Update()
+    {
+        if (HasClickE)
+        {
+            LevelVote.gameObject.SetActive(false);
+            time -= Time.deltaTime;
+            int sec = (int)time;
+            Chronotext.text = "00 : " + sec.ToString();
+            Count.text = playerReady.Count.ToString() + " / " + PhotonNetwork.PlayerList.Length.ToString();
+            WantToTeleport = playerReady.Count == PhotonNetwork.PlayerList.Length;
+            if (time <= 0f || WantToTeleport)
+            {
+                Level.gameObject.SetActive(false);
+                LevelVote.gameObject.SetActive(false);
+                Vote.gameObject.SetActive(false);
+                time = 21f;
+                if (WantToTeleport)
+                {
+                    LoadBackRoom();
+                }
+                else
+                {
+                    playerReady = new List<bool>();
+                    readyUpText.text = "Ready ?";
+                    Ready.interactable = true;
+                    LevelVote.gameObject.SetActive(true);
+                }
+                HasClickE = false;
+
+            }
+        }
+    }
+
+
+    //Teleportation
+    public void LoadBackRoom()
+    {
+        string sceneload = BossTemple.newscene;
+        PhotonNetwork.LoadLevel(sceneload);
+        Debug.Log("Room Loaded");
+
+    }
+
+
+
+    public void SendNotif()
+    {
+        base.photonView.RPC("SendMessage", RpcTarget.All);
+    }
+
+    public void VoteCanvas()
+    {
+        base.photonView.RPC("VoteRPC", RpcTarget.All);
+    }
+
+    public void OnClick_ReadyUp()
+    {
+
+        SetReadyUp(!ready);
+
+    }
+    private void SetReadyUp(bool state)
+    {
+        ready = true; //MDR cherche pas
+        if (ready)
+        {
+            readyUpText.text = "Ready !";
+            Ready.interactable = false;
+            base.photonView.RPC("PlayerReady", RpcTarget.All, ready);
+        }
+        else
+            readyUpText.text = "Ready ?";
+    }
+
+
+    [PunRPC]
+    //Envoie la notif à tout le monde.
+    void SendMessage()
+    {
+        Vote.gameObject.SetActive(true);
+    }
+
+    [PunRPC]
+    //Attend le vote de tout le monde.
+    void VoteRPC()
+    {
+        HasClickE = true;
+    }
+
+    [PunRPC]
+    void PlayerReady(bool state)
+    {
+        Debug.Log("Player ready ");
+        playerReady.Add(state);
+    }
+}
