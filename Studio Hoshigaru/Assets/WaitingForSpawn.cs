9bfd183adf0efb679a3035d12355866c9df94dd7
Pierre-Alexis VALBRUN <pierre-alexis.valbrun@epita.fr>
2021-05-30 19:27:32 +0200

    Normally UI is working

---
+++ Studio Hoshigaru/Assets/WaitingForSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class WaitingForSpawn : MonoBehaviour
+{
+    [SerializeField]PlayerRendering pr;
+    GameObject[] playersInScene;
+    Player[] playerOnline;
+
+    void Start()
+    {
+        playersInScene = GameObject.FindGameObjectsWithTag("Player");
+        playerOnline = PhotonNetwork.PlayerList;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerOnline.Length == playersInScene.Length)
+        {
+            pr.enabled = true;
+            this.enabled = false;
+        }
+        else
+        {
+            playersInScene = GameObject.FindGameObjectsWithTag("Player");
+        }
+    }
+}
