5ac2bd1ea2cf73cf40970cdc0297c6219b5e44e0
Benoit <benoit.burg@epita.fr>
2021-06-16 21:59:09 +0200

    possibilité de del des saves

---
+++ Studio Hoshigaru/Assets/Scripts/Save & Load/DeleteSave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSave : MonoBehaviour
+{
+    [SerializeField]
+    private string currentProfile;
+
+    public void Delete()
+    {
+        Debug.LogWarning(currentProfile + " delete in progress...");
+        foreach (string int_k in arrKey.int_key)
+        {
+            PlayerPrefs.DeleteKey(currentProfile + int_k);
+        }
+        foreach (string float_k in arrKey.float_key)
+        {
+            PlayerPrefs.DeleteKey(currentProfile + float_k);
+        }
+        foreach (string string_k in arrKey.string_key)
+        {
+            PlayerPrefs.DeleteKey(currentProfile + string_k);
+        }
+        Debug.LogWarning("..." + currentProfile + " deletion completed !");
+    }
+
+}
---
+++ Studio Hoshigaru/Assets/Scripts/Save & Load/arrKey.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrKey : MonoBehaviour
+{
+    public static string[] int_key = new string[]
+    { 
+        "load",
+        "nbHeart",
+        "numOfHits",
+        "defense",
+        "regen",
+        "regenTime",
+        "PlayerForce",
+        "ExtraJump",
+        "nbRunes",
+        "level",
+        "experience",
+        "point",
+        "xpForNextLevel",
+    };
+
+    public static string[] float_key = new string[]
+    {
+        "x",
+        "y",
+        "z",
+        "SpeedBoost",
+        "MovementSpeed",
+        "JumpForce",
+        "PlayerKnockback",
+
+    };
+
+    public static string[] string_key = new string[]
+    {
+        "scene",
+
+    };
+}
