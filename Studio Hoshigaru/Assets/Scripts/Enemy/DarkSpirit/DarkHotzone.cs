6c97d7206e401aecf595867dba48a729258fe3b5
Aishan mob <kerian.allaire@gmail.com>
2021-06-17 16:33:46 +0200

    Mob fumé terminé

---
+++ Studio Hoshigaru/Assets/Scripts/Enemy/DarkSpirit/DarkHotzone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkHotzone : MonoBehaviour
+{
+    private DarkSpirit enemyParent;
+    private Animator anim;
+
+    private void Awake()
+    {
+        enemyParent = GetComponentInParent<DarkSpirit>();
+        anim = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            enemyParent.TriggerArea.SetActive(true);
+            enemyParent.target = null;
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+            {
+                anim.SetBool("canWalk", false);
+            }
+        }
+    }
+}
