fb10e6264341154f1d071acbd318b4816dbb34f0
Benoit <benoit.burg@epita.fr>
2021-06-18 17:26:34 +0200

    fix
    Les portes disparaissent et les mobs ne réapparaissent plus dans TestC après le boss

---
+++ Studio Hoshigaru/Assets/Scripts/Destroy_Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Door : MonoBehaviour
+{
+    static bool created = false;
+
+    private void Awake()
+    {
+        if (!created)
+        {
+            created = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Studio Hoshigaru/Assets/Scripts/Destroy_spawnmob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Destroy_spawnmob : MonoBehaviour
+{
+    static bool created = false;
+
+    private void Awake()
+    { 
+        if (!created)
+        {
+            created = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
