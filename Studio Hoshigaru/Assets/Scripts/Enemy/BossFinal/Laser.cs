854b0c670519e2e72215ce68027590eef4f33f6c
Aishan mob <kerian.allaire@gmail.com>
2021-06-19 20:32:51 +0200

    Fix + intégration de lumière sur le boss

--- Studio Hoshigaru/Assets/Scripts/Enemy/BossFinal/Laser.cs
+++ Studio Hoshigaru/Assets/Scripts/Enemy/BossFinal/Laser.cs
@@ -5,16 +5,12 @@ using Photon.Pun;
 
 public class Laser : MonoBehaviourPun
 {
-    public int damage;
+    
     public string facingDirection;
     public MainBoss boss;
-    public float wait;
-    public float WaitMax;
-    public bool waiting;
 
     private void Start()
     {
-        wait = WaitMax;
         if (facingDirection == "left")
         {
             Vector3 theScale = transform.localScale;
@@ -22,40 +18,7 @@ public class Laser : MonoBehaviourPun
             transform.localScale = theScale;
         }
     }
-    private void Update()
-    {
-        if(waiting)
-        {
-            Wait();
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player")&& !waiting)
-        {
-            GameObject player = collision.gameObject;
-            player.GetComponent<Health>().numOfHits -= damage;
-            waiting = true;
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && !waiting)
-        {
-            GameObject player = collision.gameObject;
-            player.GetComponent<Health>().numOfHits -= damage;
-            waiting = true;
-        }
-    }
-    public void Wait()
-    {
-        wait -= Time.deltaTime;
-        if (wait <= 0)
-        {
-            wait = WaitMax;
-            waiting = false;
-        }
-    }
+
 
     public void EndLaser()
     {
