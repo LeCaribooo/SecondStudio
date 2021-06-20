5f79e6106c81f50f89464b1c8ad002c4696de6bb
Benoit <benoit.burg@epita.fr>
2021-06-20 18:12:45 +0200

    Saucisse de Strasbourg à la béchamel

--- Studio Hoshigaru/Assets/Scripts/SoundControl.cs
+++ Studio Hoshigaru/Assets/Scripts/SoundControl.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundControl : MonoBehaviour
 {
     [Range(0.0f, 1.0f)]
-    
+
     static float masterVolume = 1.0f;
 
+    public Slider GetValue;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("soundValue"))
+        {
+            AudioListener.volume = PlayerPrefs.GetFloat("soundValue");
+            Debug.LogWarning("Volume audio : " + AudioListener.volume);
+            masterVolume = PlayerPrefs.GetFloat("soundValue");
+            GetValue.value = masterVolume;
+        }
+    }
 
     void Update()
     {
