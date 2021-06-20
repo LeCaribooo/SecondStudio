e09c1985d70593b8a6bad102d79fbf80080d606d
Benoit <benoit.burg@epita.fr>
2021-06-20 07:23:23 +0200

    Choucroute
    A merge avec la ratatouille de Kerian

---
+++ Studio Hoshigaru/Assets/Scripts/Active_this_portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Active_this_portal : MonoBehaviour
+{
+
+    static bool enable;
+    public void enablle()
+    {
+
+        enable = true;
+
+    }
+
+    private void Update()
+    {
+        if (enable)
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+
+
+}
