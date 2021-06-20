5ed2e01b063552332b6218457586ca221cd5e118
Benoit <benoit.burg@epita.fr>
2021-06-20 19:18:11 +0200

    Un saucisson de qualité

---
+++ Studio Hoshigaru/Assets/Scripts/QualityChoice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QualityChoice : MonoBehaviour
+{
+    static int tmp;
+
+    public Dropdown Quality;
+
+    public void Awake()
+    {
+        int t = PlayerPrefs.GetInt("QualitySetting");
+        QualitySettings.SetQualityLevel(t);
+        Quality.value = t;
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        tmp = qualityIndex;
+        QualitySettings.SetQualityLevel(tmp);
+    }
+
+    public void SaveQuality()
+    {
+        PlayerPrefs.SetInt("QualitySetting", tmp);
+    }
+}
