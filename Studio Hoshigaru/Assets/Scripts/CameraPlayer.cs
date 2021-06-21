0dee39ad81c2629c1600b840a2fcb4b78ae2c479
Aishan mob <kerian.allaire@gmail.com>
2021-06-21 03:51:17 +0200

    Caméra

--- Studio Hoshigaru/Assets/Scripts/CameraPlayer.cs
+++ Studio Hoshigaru/Assets/Scripts/CameraPlayer.cs
@@ -7,9 +7,16 @@ public class CameraPlayer : MonoBehaviour
     public Transform target;
     public float smoothSpeed;
     public Vector3 offset;
+    public bool check = true;
 
     private void LateUpdate()
     {
+        GameObject boss = GameObject.FindGameObjectWithTag("BossF");
+        if(boss != null && check)
+        {
+            gameObject.GetComponent<Camera>().orthographicSize = (8);
+            check = false;
+        }
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
