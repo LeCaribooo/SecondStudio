f367bdbf8dc7141895d21e2f25d2c9edb5350030
Aishan mob <kerian.allaire@gmail.com>
2021-06-21 16:01:57 +0200

    Audio sur toutes les scènes

---
+++ Studio Hoshigaru/Assets/Resources/musics/GolemFade.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemFade : MonoBehaviour
+{
+    public AudioClip[] musics;
+    private float volume;
+    private bool check = false;
+    public Golem boss;
+    public bool end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = 0f;
+        boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<Golem>();
+        GetComponent<AudioSource>().volume = volume;
+        GetComponent<AudioSource>().clip = musics[0];
+        GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (boss.deade && !check)
+        {
+            if (GetComponent<AudioSource>().volume > 0.00)
+            {
+                volume -= 0.0005f;
+                GetComponent<AudioSource>().volume = volume;
+
+            }
+            else
+            {
+                GetComponent<AudioSource>().Stop();
+                GetComponent<AudioSource>().clip = musics[1];
+                GetComponent<AudioSource>().Play();
+                check = true;
+            }
+        }
+        else if(check && !end)
+        {
+            if (GetComponent<AudioSource>().volume < 1)
+            {
+                volume += 0.001f;
+                GetComponent<AudioSource>().volume = volume;
+            }
+            else
+            {
+                end = true;
+            }
+        }
+    }
+}
